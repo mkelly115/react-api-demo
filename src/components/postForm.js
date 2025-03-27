04683023b5dba193759b9825a770987523baea2f
Michael <mikek222@gmail.com>
2025-03-27 16:26:09 -0400

    updated addPost

--- src/components/postForm.js
+++ src/components/postForm.js
@@ -1,6 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { createPosts } from "../services/postService";
 
-export default function PostForm({posts, setPosts}){
+export default function PostForm({ posts, setPosts }) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-    return(<h1>Create Post</h1>)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    addPost();
+    setTitle('');
+    setBody('');
+
+  }
+
+  const addPost = () => {
+    createPosts({title, body})
+    .then(() => {
+        
+    })
+    .catch(err => {
+        console.error(err)
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>Title</div>
+      <input
+        value={title}
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <div>Body</div>
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+      <div>
+      <button type="submit">Add Post</button>
+      </div>
+    </form>
+  );
 }
